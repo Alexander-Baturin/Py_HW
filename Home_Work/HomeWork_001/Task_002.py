6e5c269123530197b099649bbce4959b83727222
«Aleksandr» <AlexusB92@yandex.ru>
2022-11-30 21:17:07 +0300

    Add Task_002

---
+++ Home_Work/HomeWork_001/Task_002.py
@@ -0,0 +1,11 @@
+# Задача 2. Напишимте программу для проверки истинности утверждения ¬X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+
+x = int(input('Введите X - '))
+y = int(input('Введите Y - '))
+z = int(input('Введите Z - '))
+
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
+            print(not (x | y | z) == (not x and not y and not z))
+            print(x, y, z)
