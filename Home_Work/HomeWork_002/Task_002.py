1d69fc5bc43667e97af36f8cab0dd1b07cccf766
«Aleksandr» <AlexusB92@yandex.ru>
2022-12-05 20:23:08 +0300

    First correct Task_002

--- Home_Work/HomeWork_002/Task_002.py
+++ Home_Work/HomeWork_002/Task_002.py
@@ -8,22 +8,23 @@ for i in range(1, N+1):
     my_list.append(f'{k}')
 
 print(my_list)
+print()
 
-# def str_to_num(my_list):
-#     str = str.strip()
-#     if '.' in str and str.replace('.', '').isdigit():
-#         return float(str)
-#     elif str.isdigit():
-#         return int(str)
+#print(sum(my_list,1))
+def my_list_to_num(str):
+    str = str.strip()
+    if '.' in str and str.replace('.', '').isdigit():
+        return float(str)
+    elif str.isdigit():
+        return int(str)
 
-# num_list = []
-# for i in my_list:
-#     n = str_to_num(i)
-#     if n is not None:
-#         num_list.append(str_to_num(i))
-#
-# print(num_list)
-#
-# sum(num_list)
-# sum(my_list)
-# print({sum()})
+num_list = []
+for i in my_list:
+    n = my_list_to_num(i)
+    if n is not None:
+        num_list.append(my_list_to_num(i))
+
+print(num_list)
+print()
+#sum(num_list,1)
+print(sum(num_list,1))
