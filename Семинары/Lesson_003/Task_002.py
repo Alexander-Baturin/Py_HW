a43b101b8d147b68b2000b50bc1d3d139ebea70d
«Aleksandr» <AlexusB92@yandex.ru>
2022-12-14 15:04:58 +0300

    Correct task_002

--- Семинары/Lesson_003/Task_002.py
+++ Семинары/Lesson_003/Task_002.py
@@ -4,10 +4,12 @@
 my_list = ['qwe', 'asd', 'zxc', 'qwe', 'dfg', 'cvb']
 search_string = input('Ведите символ: ')
 
-count = None
-for item in my_list:
-    for i in range(len(my_list)):
-        if search_string == item[i]:
-            count += i
-        if count == 2:
-            print(f'список - {my_list}, ищем :{search_string}, ответ: {i}')
+count = 0
+for i in range(len(my_list)):
+    if my_list[i] == search_string:
+        count += 1
+    if count == 2:
+        print(f'список - {my_list}, ищем :{search_string}, ответ: {i}')
+        break
+else:
+    print(f'список - {my_list}, ищем :{search_string}, ответ: False')
