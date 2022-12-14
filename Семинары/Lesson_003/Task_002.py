ec81d646c3b47b46c30152cf98f8d4395cd20ef6
«Aleksandr» <AlexusB92@yandex.ru>
2022-12-14 14:51:38 +0300

    Correct task_002

--- Семинары/Lesson_003/Task_002.py
+++ Семинары/Lesson_003/Task_002.py
@@ -1,4 +1,13 @@
 # Задача 2. Напишите программу, которая определит позицию второго вхождения строки в списке (полное соответствие)
 # либо сообщит, что ее нет.
 
+my_list = ['qwe', 'asd', 'zxc', 'qwe', 'dfg', 'cvb']
+search_string = input('Ведите символ: ')
 
+count = None
+for item in my_list:
+    for i in range(len(my_list)):
+        if search_string == item[i]:
+            count += i
+        if count == 2:
+            print(f'список - {my_list}, ищем :{search_string}, ответ: {i}')
