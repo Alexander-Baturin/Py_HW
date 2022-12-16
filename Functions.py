81b1dcde81af2e850f8591b5d07305e9c454b530
«Aleksandr» <AlexusB92@yandex.ru>
2022-12-16 20:54:08 +0300

    Added new information to the Functions

--- Functions.py
+++ Functions.py
@@ -4,3 +4,10 @@ def MaxIntList(list):
         if max < list[i]:
             max = list[i]
     return max
+
+def MinIntList(list):
+    min = list[0]
+    for i in range(1, len(list)):
+        if min > list[i]:
+            min = list[i]
+    return min
