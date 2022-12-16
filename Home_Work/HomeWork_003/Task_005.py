894862bf896672aafc1dcc7fca9bc4b876b22abe
«Aleksandr» <AlexusB92@yandex.ru>
2022-12-16 15:30:17 +0300

    Correct 1 Task_005

--- Home_Work/HomeWork_003/Task_005.py
+++ Home_Work/HomeWork_003/Task_005.py
@@ -2,13 +2,14 @@
 # Пример: для k = 8 список будет выглядеть так: [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21]
 
 n = int(input('Введите число N: '))
+
 fib_1 = 1
 fib_2 = 1
 nfib_1 = 1
 nfib_2 = -1
 k = n
-# m = -k
 i = 0
+
 my_list = []
 my_list_fib = []
 my_list_nfib = []
@@ -24,7 +25,7 @@ def fibonacci(n):
         return 1
     return fibonacci(n - 1) + fibonacci(n - 2)
 
-for i in range(len(my_list)):
+for i in my_list:
     fibonacci(n)
     my_list_fib.append(fibonacci(n))
     n -= 1
@@ -35,25 +36,24 @@ my_list_fib.append(0)
 
 # print(my_list_fib)
 my_list_fib.reverse()
+# print(my_list_fib)
 
-my_list.reverse()
+# my_list.reverse()
 # print(my_list)
 # print()
 
 # def nfibonacci(n):
-#     if n in (-1, 1):
+#     if n in (1, 2):
 #         return 1
 #     return nfibonacci(n + 2) - nfibonacci(n + 1)
-#
-# for i in range(len(my_list)):
+
+# for i in my_list:
 #     nfibonacci(n)
 #     my_list_nfib.append(nfibonacci(n))
 #     n += 1
-#     i -= 1
-#
+#     # i -= 1
 # print(my_list_nfib)
 
-my_end_list = my_list_fib
-my_end_list.extend(my_list_nfib)
+my_end_list = my_list_nfib + my_list_fib
 
 print(f'Для N = {k} список будет выглядеть так: {my_end_list}')
