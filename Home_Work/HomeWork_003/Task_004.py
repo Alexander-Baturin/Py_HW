9686bb2f6409c52f7aadc0b55f4769c9fcc308aa
«Aleksandr» <AlexusB92@yandex.ru>
2022-12-21 17:49:56 +0300

    Correct Task_004

--- Home_Work/HomeWork_003/Task_004.py
+++ Home_Work/HomeWork_003/Task_004.py
@@ -7,16 +7,22 @@
 
 n = int(input('Введите число: '))
 
-my_list = []
+# my_list = []
+# while n > 0:
+#     if n % 2 != 0:
+#         my_list.insert(0, 1)
+#         n = n//2
+#
+#     else:
+#         my_list.insert(0, 0)
+#         n = n // 2
+#
+# s = ''.join(map(str,my_list))
+#
+# print(f"{n} -> {s}")
+
+bi_num = ''# вариант решения после разбора дз
 while n > 0:
-    if n % 2 != 0:
-        my_list.insert(0, 1)
-        n = n//2
-
-    else:
-        my_list.insert(0, 0)
-        n = n // 2
-
-s = ''.join(map(str,my_list))
-
-print(f"{n} -> {s}")
+    bi_num = str(n % 2) + bi_num
+    n //= 2
+print(bi_num)
