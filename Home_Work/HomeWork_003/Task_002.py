53f60bb452760c24058d7f1b6e49619c22d1e2e7
«Aleksandr» <AlexusB92@yandex.ru>
2022-12-21 17:28:22 +0300

    Correct Task_002

--- Home_Work/HomeWork_003/Task_002.py
+++ Home_Work/HomeWork_003/Task_002.py
@@ -17,13 +17,25 @@ for i in range(size):
 # print()
 
 
-new_list = []
-k = -1
+# new_list = []# мой вариант решения (не работает при списке более 5 элементов)
+# k = -1
+#
+# for i in range(len(rnd_list)):
+#     if i <= (len(rnd_list) % 2) + 1:
+#         new_list.append(rnd_list[i] * rnd_list[k])
+#         i += 1
+#         k -= 1
+#
+# print(f'{rnd_list} => {new_list}')
 
-for i in range(len(rnd_list)):
-    if i <= (len(rnd_list) % 2) + 1:
-        new_list.append(rnd_list[i] * rnd_list[k])
-        i += 1
-        k -= 1
+if size % 2 == 0:
+    center = int(len(rnd_list)/2)
+else:
+    center = int(len(rnd_list) / 2 + 1)
+
+new_list = []
+
+for i in range(center):
+    new_list.append(rnd_list[i] * rnd_list[len(rnd_list) - 1 - i])
 
 print(f'{rnd_list} => {new_list}')
