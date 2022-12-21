8d0a1da0fa5962f6b8dbd619cd43cfb6857bfcec
«Aleksandr» <AlexusB92@yandex.ru>
2022-12-21 17:08:09 +0300

    Correct Task_003

--- Home_Work/HomeWork_002/Task_003.py
+++ Home_Work/HomeWork_002/Task_003.py
@@ -12,12 +12,22 @@ for i in range(size):
 print(rnd_list)
 print()
 
-# com_set = itertools.permutations(rnd_list, size)
+# com_set = itertools.permutations(rnd_list, size)# вариант подсмотренный в интернете
 # for i in com_set:
 #     print(i)
 
-k = -1
-for i in range(len(rnd_list)//2):
-    rnd_list[k], rnd_list[i] = rnd_list[i], rnd_list[k]
-    print(rnd_list)
-    k -= 1
+# k = -1
+# for i in range(len(rnd_list)//2):
+#     rnd_list[k], rnd_list[i] = rnd_list[i], rnd_list[k]
+#     print(rnd_list)
+#     k -= 1
+
+for i in range(len(rnd_list)):# решение 1 после разбора дз на семенаре
+    k = random.randint(0, (len(rnd_list) - 1))
+    rnd_list[i], rnd_list[k] = rnd_list[k], rnd_list[i]
+print(rnd_list)
+
+for i in range(len(rnd_list)):# решение 2 после разбора дз на семенаре
+    k = random.randint(0, (len(rnd_list) - 1))
+    rnd_list.append(rnd_list.pop(k))
+print(rnd_list)
